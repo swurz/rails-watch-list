58fa8b34e64bb7e30c8eaa74949d6570f425090f
Simone Wurz <simonewurz@gmail.com>
2023-08-17 16:24:34 +0200

    watch list

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,4 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
 
   has_many :movies, through: :bookmarks, dependent: :destroy
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # LISTS
+  # A user can see all the lists
+  #get "lists", to: "lists#index"
+  # A user can see the details of a given list and its name
+  #get "lists/:id", to: "lists#show"
+  # A user can create a new list
+  #get "lists/new", to: "lists#new"
+  #ost "lists", to: "lists#create"
+
+  resources :lists
 end
